1898b8dced29e95edf5588432abf17af9a6970fe
Koyo Nabeta <107094464+nabesanta@users.noreply.github.com>
2023-10-09 16:10:22 +0900

    Update data_collect.py

--- scripts/data_collect.py
+++ scripts/data_collect.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 import sys
@@ -8,68 +8,59 @@ from sensor_msgs.msg import JointState
 import csv
 from std_msgs.msg import String
 
-
 POSITIONS_LIST = []
 LABEL = None
 COUNT = 0
 PREV = None
 
-
 '''
-	Callback function when a joint_states message is published.
-	Adds positions to global list to be processed right afterwards.
+    Callback function when a joint_states message is published.
+    Adds positions to global list to be processed right afterwards.
 '''
 def control_command(val):
-	global POSITIONS_LIST
-	global LABEL
-	global PREV
-	global TASK_COUNT
+    global POSITIONS_LIST
+    global LABEL
+    global PREV
+    global TASK_COUNT
 
-	if list(val.position) == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]:
-		return
+    if list(val.position) == [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]:
+        return
 
-	if LABEL:
-		if PREV != list(val.position):
-			POSITIONS_LIST.append(list(val.position) + [LABEL])
-			PREV = list(val.position)
-	else:
-		if PREV != list(val.position):
-			POSITIONS_LIST.append(list(val.position))
-			PREV = list(val.position)
-
+    if LABEL:
+        if PREV != list(val.position):
+            POSITIONS_LIST.append(list(val.position) + [LABEL])
+            PREV = list(val.position)
+    else:
+        if PREV != list(val.position):
+            POSITIONS_LIST.append(list(val.position))
+            PREV = list(val.position)
 
 '''
-	Helper for writing positions to csv
+    Helper for writing positions to csv
 '''
 def collect(out):
-
-	while POSITIONS_LIST:
-		out.writerow(POSITIONS_LIST[0])
-		POSITIONS_LIST.pop(0)
-
+    while POSITIONS_LIST:
+        out.writerow(POSITIONS_LIST[0])
+        POSITIONS_LIST.pop(0)
 
 if __name__ == '__main__':
+    print('collect - ', sys.argv)
 
-	print 'collect - ', sys.argv
+    rospy.init_node('data_collector', anonymous=True)
+    rospy.Subscriber(sys.argv[1],
+                    JointState,
+                    control_command)
 
-	rospy.init_node('data_collector', anonymous=True)
-	rospy.Subscriber(sys.argv[1],
-				JointState,
-				control_command)
+    # Argv 1 - path + file name is expected
+    cwd = sys.argv[2]
 
+    # NOTE: The label is an option
+    if len(sys.argv) > 3:
+        LABEL = sys.argv[3]
 
-	# Argv 1 - path + file name is expected
-	cwd = sys.argv[2]
-
-	# NOTE: The label is an option
-	if len(sys.argv) > 3:
-		LABEL = sys.argv[3]
-
-	sleep(3)
-	with open(cwd, "w+") as out_file:
-
-		# print out_file
-		out = csv.writer(out_file, delimiter=',')
-		print 'Writing to', cwd
-		while not rospy.is_shutdown():
-			collect(out)
+    sleep(3)
+    with open(cwd, "w+") as out_file:
+        out = csv.writer(out_file, delimiter=',')
+        print('Writing to', cwd)
+        while not rospy.is_shutdown():
+            collect(out)
