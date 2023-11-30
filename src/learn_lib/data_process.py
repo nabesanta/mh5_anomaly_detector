82fe720b0f01e930e069512251368f8f3a511e7e
Koyo Nabeta <107094464+nabesanta@users.noreply.github.com>
2023-11-30 17:34:41 +0900

    Update data_process.py

--- src/learn_lib/data_process.py
+++ src/learn_lib/data_process.py
@@ -1,104 +1,78 @@
-'''
-ROS Anomaly Detector Framework
-
-Author:
-	Vedanth Narayanan
-File:
-	Data Process class
-Date:
-	4 May, 2018
-'''
-
 import os
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 from sklearn.utils import shuffle
 import numpy as np
-from sklearn.externals import joblib
+import joblib
 import pandas as pd
 
 
-class DataProcess(object):
-	"""
-		Specifically for processing data and getting it ready
-			for anomaly detection.
-	"""
+class DataProcess:
+    """
+    Specifically for processing data and getting it ready
+    for anomaly detection.
+    """
 
-	def __init__(self, comps):
-		self.__scale = MinMaxScaler(feature_range=(0, 1))
-		self.__reduction_size = comps
+    def __init__(self, comps):
+        self.__scale = MinMaxScaler(feature_range=(0, 1))
+        self.__reduction_size = comps
 
-		if comps > 0:
-			self.__reduction = PCA(n_components=comps)
-		else:
-			self.__reduction = None
+        if comps > 0:
+            self.__reduction = PCA(n_components=comps)
+        else:
+            self.__reduction = None
 
+    def split(self, in_file, x_cols, y_cols):
+        ''' Read csv, to dataframe, split to x and y. '''
+        dataframe = pd.read_csv(in_file, header=None, engine='python')
+        dataset = dataframe.values
+        data = shuffle(dataset)
 
-	def split(self, in_file, x_cols, y_cols):
-		''' Read csv, to dataframe, split to x and y. '''
+        # Gets rid of redundant entries, more accurate
+        data = np.unique(data, axis=0)
 
+        dataframe_x = data[:, x_cols]
+        dataframe_y = data[:, y_cols]
 
-		dataframe = pd.read_csv(in_file, header=None, engine='python')
-		dataset = dataframe.values
-		data = shuffle(dataset)
+        return dataframe_x, dataframe_y
 
-		# Gets rid of redundant entries, more accuracte
-		data = np.unique(data, axis=0)
+    def scaler_fit(self, data):
+        ''' Fit scaling object '''
+        # NOTE: If model exists, load. or fit and save
+        saveloc = '../data/scale.pkl'
+        if os.path.exists(saveloc):
+            self.__scale = joblib.load(saveloc)
+            data = self.__scale.transform(data)
+        else:
+            data = self.__scale.fit_transform(data)
+            joblib.dump(self.__scale, saveloc)
+        return data
 
-		dataframe_x = data[:, x_cols]
-		dataframe_y = data[:, y_cols]
+    def scaler_transform(self, data):
+        ''' Data supplied is scaled and returned. '''
+        transformed_data = self.__scale.transform(data)
+        return transformed_data
 
-		return dataframe_x, dataframe_y
+    def reduction_fit(self, data):
+        ''' Fit PCA decomposition. '''
+        # NOTE: If model exists, load. or fit and save
+        if self.__reduction:
+            saveloc = '../data/reduce.pkl'
+            if os.path.exists(saveloc):
+                self.__reduction = joblib.load(saveloc)
+                data = self.__reduction.transform(data)
+            else:
+                data = self.__reduction.fit_transform(data)
+                joblib.dump(self.__reduction, saveloc)
+        return data
 
+    def reduction_transform(self, data):
+        ''' Reduce dimensionality of given data. '''
+        if self.__reduction:
+            transformed_data = self.__reduction.transform(data)
+            return transformed_data
+        return data
 
-	def scaler_fit(self, data):
-		''' Fit scaling object '''
-		# NOTE: If model exists, load. or fit and save
-
-		# print os.getcwd()
-		saveloc = '../data/scale.pkl'
-		if os.path.exists(saveloc):
-			self.__scale = joblib.load(saveloc)
-			data = self.__scale.transform(data)
-		else:
-			data = self.__scale.fit_transform(data)
-			joblib.dump(self.__scale, saveloc)
-
-		return data
-
-
-	def scaler_transform(self, data):
-		''' Data supplied is scaled and returned. '''
-		transformed_data = self.__scale.transform(data)
-		return transformed_data
-
-
-	def reduction_fit(self, data):
-		''' Fit PCA decomposition. '''
-		# NOTE: If model exists, load. or fit and save
-
-		if self.__reduction:
-			saveloc = '../data/reduce.pkl'
-			if os.path.exists(saveloc):
-				self.__reduction = joblib.load(saveloc)
-				data = self.__reduction.transform(data)
-			else:
-				data = self.__reduction.fit(data)
-				joblib.dump(self.__reduction, saveloc)
-
-		return data
-
-
-	def reduction_transform(self, data):
-		''' Reduce dimensionality of given data. '''
-
-		if self.__reduction:
-			transformed_data = self.__reduction.transform(data)
-			return transformed_data
-		return data
-
-
-	def get_reduction_size(self):
-		''' Return number of components passed to PCA'''
-
-		return self.__reduction_size
+    def get_reduction_size(self):
+        ''' Return number of components passed to PCA'''
+        return self.__reduction_size
